76b580d9e258dc73c6c1a967b01e6e4b52b4272f
AyaTaka <ayataka2591@gmail.com>
2024-10-30 07:56:52 +0900

    コンプレッサマップを生成するスクリプトを追加

--- Chk002_cmp_map_generater.m
+++ Chk002_cmp_map_generater.m
@@ -1,15 +1,7 @@
 clear all
-close all
+%close all
 
 %% パラメータの初期設定
-% Initial Condition
-T_init = 293.15;    % [K]
-P_init = 101325;    % [Pa]
-
-% Boundary Condition
-T_amb = 293.15;    % [K]
-P_amb = 101325;    % [Pa]
-
 % Set Compressor Parameter
 cmp.param.dc = 0.2286;      % [m]
 cmp.param.eta_is = 0.8;     % [-]
@@ -29,27 +21,29 @@ kap = cmp.ref.kap;  % [-]
 cp = cmp.ref.cp;    % [J/kg/K]
 
 %% コンプレッサマップの作成
-N_table = [10000:10000:100000];
-pr = [1:0.1:4];
-p_in = P_amb;
-T_in = T_amb;
+N_table = linspace(10000,100000,10);
+pr = linspace(1,4,100);
 T_ref = cmp.ref.T;
 R_ref = cmp.ref.R;
 p_ref = cmp.ref.p;
 
 Wcr = zeros(size(N_table,2),size(pr,2));
-T_out = zeros(size(N_table,2),size(pr,2));
-torque = zeros(size(N_table,2),size(pr,2));
 
-for i = 1:size(N_table,2)
-    N = N_table(i);
-    for j= 1:size(pr,2)
-        p_out = p_in *pr(j);
-        sim(".\SLX\Simple_Compressor.slx");
-        Wcr(i,j) = ans.W_cmp(end);
-        T_out(i,j) = ans.T_out(end);
-        torque(i,j) = ans.torque(end);
-    end
+Uc = N_table * pi/30 * (cmp.param.dc/2);
+MUc = Uc / sqrt(cmp.ref.kap*cmp.ref.R*cmp.ref.T);
+a = cmp.map.coeff.a;
+b = cmp.map.coeff.b;
+c = cmp.map.coeff.c;
+phi_max = a(1)*MUc.^4 + a(2)*MUc.^3 + a(3)*MUc.^2 + a(4)*MUc + a(5);
+beta = b(1)*MUc.^2 + b(2)*MUc + b(3);
+MUy_max = c(1)*MUc.^5 + c(2)*MUc.^4 + c(3)*MUc.^3 + c(4)*MUc.^2 + c(5)*MUc + c(6);
+
+MUy = zeros(size(Uc,2),size(pr,2));
+phi = zeros(size(Uc,2),size(pr,2));
+for i = 1:size(Uc,2)
+    MUy(i,:) = cp*T_ref*(pr.^((kap-1)/kap)-1) / (Uc(i)^2/2);
+    phi(i,:) = phi_max(i) * (1 -  exp((MUy(i,:)/MUy_max(i) -1)*beta(i)));
+    Wcr(i,:) = phi(i,:) * Uc(i) * p_ref/(R_ref*T_ref) * pi/4*cmp.param.dc^2;
 end
 
 %% Compressor P-Q Map
@@ -66,25 +60,17 @@ xlim([0 0.1]);
 xlabel("Corrected mass flow rate [kg/s]");
 ylabel("Pressure ratio");
 
-%% 出口温度 vs 圧力比
-figure('Name','Outlet Temperature vs Pressure Ratio')
+figure('Name','Compressor non-dimensional Map')
 hold on
 grid on
 box on
 
 for i = 1:size(N_table,2)
-    plot(T_out(i,:),pr);
+    plot(phi(i,:),MUy(i,:));
 end
-xlabel("Temrature [K]");
-ylabel("Pressure [Pa]");
-%% torque vs 圧力比
-figure('Name','Torque vs Pressure Ratio')
-hold on
-grid on
-box on
 
-for i = 1:size(N_table,2)
-    plot(torque,pr);
-end
-xlabel("Temrature [K]");
-ylabel("Pressure [Pa]");
+xlim([0 max(phi,[],"all")]);
+ylim([0 0.4])
+xlabel("Flow coefficient [-]");
+ylabel("Pressure coefficient [-]");
+
